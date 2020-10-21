16da4ff32ca552fa1192aa87dc4c955969c2b89c
dingshimin <dingshimin@jddmote.com>
2020-10-21 16:31:14 +0800

    Add io.github.shallowinggg.parent as pom parent

--- spiral-spring-boot-autoconfigure/src/main/java/io/github/shallowinggg/boot/autoconfigure/SpiralAutoConfiguration.java
+++ spiral-spring-boot-autoconfigure/src/main/java/io/github/shallowinggg/boot/autoconfigure/SpiralAutoConfiguration.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright © 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.github.shallowinggg.boot.autoconfigure;
 
+import io.github.shallowinggg.spiral.config.SpiralConstant;
 import io.github.shallowinggg.spiral.spring.EnableSpiral;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 /**
  * @author ding shimin
@@ -13,8 +30,27 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(SpiralProperties.class)
 public class SpiralAutoConfiguration {
 
-    @EnableSpiral
-    @Configuration
-    public static class Config {
-    }
+	private final ConfigurableEnvironment env;
+
+	public SpiralAutoConfiguration(ConfigurableEnvironment env) {
+		this.env = env;
+	}
+
+	@Bean
+	public SpiralProperties spiralProperties() {
+		SpiralProperties spiralProperties = new SpiralProperties();
+		boolean enable =
+				env.getProperty(SpiralConstant.ENABLE_PROPERTY, Boolean.class, Boolean.FALSE);
+		String tag = env.getProperty(SpiralConstant.TAG_PROPERTY);
+		spiralProperties.setEnable(enable);
+		if (enable) {
+			spiralProperties.setTag(tag);
+		}
+		return spiralProperties;
+	}
+
+	@EnableSpiral
+	@Configuration
+	public static class Config {
+	}
 }
--- spiral-spring-boot-autoconfigure/src/main/java/io/github/shallowinggg/boot/autoconfigure/SpiralProperties.java
+++ spiral-spring-boot-autoconfigure/src/main/java/io/github/shallowinggg/boot/autoconfigure/SpiralProperties.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package io.github.shallowinggg.boot.autoconfigure;
 
 import io.github.shallowinggg.spiral.config.SpiralConstant;
@@ -12,28 +26,28 @@ import org.springframework.util.Assert;
 @ConfigurationProperties(prefix = SpiralProperties.SPIRAL_PREFIX)
 public class SpiralProperties {
 
-    public static final String SPIRAL_PREFIX = "spiral";
+	public static final String SPIRAL_PREFIX = "spiral";
 
-    private String tag;
+	private String tag;
 
-    private boolean enable = false;
+	private boolean enable = false;
 
-    public String getTag() {
-        return tag;
-    }
+	public String getTag() {
+		return tag;
+	}
 
-    public void setTag(String tag) {
-        Assert.hasText(tag, "tag must has text");
-        this.tag = tag;
-        System.setProperty(SpiralConstant.TAG_PROPERTY, tag);
-    }
+	public void setTag(String tag) {
+		Assert.hasText(tag, "tag must has text");
+		this.tag = tag;
+		System.setProperty(SpiralConstant.TAG_PROPERTY, tag);
+	}
 
-    public boolean isEnable() {
-        return enable;
-    }
+	public boolean isEnable() {
+		return enable;
+	}
 
-    public void setEnable(boolean enable) {
-        this.enable = enable;
-        System.setProperty(SpiralConstant.ENABLE_PROPERTY, String.valueOf(enable));
-    }
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+		System.setProperty(SpiralConstant.ENABLE_PROPERTY, String.valueOf(enable));
+	}
 }
